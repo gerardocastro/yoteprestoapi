d596de2f23fdf0a9c7a705d96a095d6bd1b62d98
Gerardo Castro <gerardo_ct@hotmail.com>
2016-07-21 14:03:36 -0500

    add default json to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: 'json' }  do
     scope :v1 do
-      mount_devise_token_auth_for "User", at: 'auth', controllers: {
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
           omniauth_callbacks: 'overrides/omniauth_callbacks',
           sessions:           'overrides/sessions'
         }
