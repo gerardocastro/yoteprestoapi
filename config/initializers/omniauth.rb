6c27e39bc4919ccb8152b0bb41f720db5f77ed77
Gerardo Castro <gerardo_ct@hotmail.com>
2016-07-23 11:08:52 -0500

    change facebook omniuth callback url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, callback_url: "http://localhost:3000/omniauth/facebook/callback"
+  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, callback_url: "https://yoteprestoapi.herokuapp.com/omniauth/facebook/callback"
 end
